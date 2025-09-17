142c68a312fa091f07a2ce4854a3bd6a52b7500f
George Myrianthous <g.myrianthous@withplum.com>
2025-09-17 10:18:22 +0100

    chore: add test client in conftest

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,2 +1,16 @@
+from collections.abc import AsyncGenerator
+
+import pytest
+from httpx import ASGITransport
+from httpx import AsyncClient
+
+from ai_assistant.api.main import app
 from tests.fixtures.database import *  # noqa
 from tests.fixtures.repository import *  # noqa
+
+
+@pytest.fixture(scope='function')
+async def test_client() -> AsyncGenerator[AsyncClient, None]:
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url='http://test') as client:
+        yield client
